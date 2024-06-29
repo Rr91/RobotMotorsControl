c5a04583fe18669111b58c6f501425ac03cbc4ae
Rr91 <romochka-remnev@mail.ru>
2024-06-29 12:24:38 +0300

    code

--- motors_control.py
+++ motors_control.py
@@ -1,1 +1,17 @@
-test
+
+class Robot:
+    
+    def __init__(self, name):
+    	self.name = name
+    	
+    def move_front(self):
+    	print(f'я {self.name} двигаюсь вперед')
+    	
+    def move_back(self):
+    	print(f'я {self.name} двигаюсь назад')
+
+rob1 = Robot('Вася')
+rob1.move_front()
+rob1.move_back()
+
+    	
