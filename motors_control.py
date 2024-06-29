b492facfc7628f06f15707987e98047d91ef20c4
Rr91 <romochka-remnev@mail.ru>
2024-06-29 12:37:52 +0300

    add turn function

--- motors_control.py
+++ motors_control.py
@@ -9,9 +9,16 @@ class Robot:
     	
     def move_back(self):
     	print(f'я {self.name} двигаюсь назад')
+    
+    def turn_left(self):
+        print(f'я {self.name} повернул налево')
+    
+    def turn_right(self):
+        print(f'я {self.name} повернул направо')
 
 rob1 = Robot('Вася')
 rob1.move_front()
+rob1.turn_left()
 rob1.move_back()
 
     	
